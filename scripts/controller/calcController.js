aa93ebd01d8c7aac240bba1d9dbdfe7226f367a5
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-03-19 18:45:49 -0300

    feat: add date function and configure date and time format to pt-BR"

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,27 +1,68 @@
 class CalcController {
 
-    constructor(){
+  constructor() {
+    this._locale = 'pt-BR';
+    this._displayCalcEl = document.querySelector("#display");
+    this._dateEl = document.querySelector("#data");
+    this._timeEl = document.querySelector("#hora");
+    this._currentDate;
+    this.initialize();
 
-     this.displayCalc = "0";
-     this._dataAtual;
+  }
+
+  initialize(){
+
+       this.setDisplayDateTime();        
         
+    setInterval(()=>{
+
+        this.setDisplayDateTime();
         
+    }, 1000);
+
 }
 
-get displayCalc(){
-    return this._displayCal;
+        setDisplayDateTime(){
+
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+        });
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+
+    }
+
+  
+
+get displayTime(){
+return this._timeEl.innerHTML;
 }
 
-set displayCalc(Valor){
-    this._displayCalc = Valor;
+set displayTime(value){
+return this._timeEl.innerHTML = value;
 }
 
-get dataAtual(){
-    return this._dataAtual;
+get displayDate() {
+    return this._dateEl.innerHTML;
+    }
+
+set displayDate (value) {
+return this._dateEl.innerHTML = value;
+
 }
 
-set dataAtual(valor){
-    this._datalAtual = valor;
-}
 
+  set displayCalc(value) {
+    this._displayCalcEl.innerHTML = value;
+  }
+
+  get currentDate() {
+  return new Date();
+  }
+
+  set currentDate(value) {
+
+    this._currentDate = value;
+  }
 }
