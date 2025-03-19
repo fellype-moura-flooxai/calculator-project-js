ea93243fa0641bf08fadcf62280e67adac3a43f5
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-03-19 16:57:33 -0300

    feat: add constructor method, encapsulation, and implement get/set

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,4 +1,27 @@
 class CalcController {
 
-    
+    constructor(){
+
+     this.displayCalc = "0";
+     this._dataAtual;
+        
+        
+}
+
+get displayCalc(){
+    return this._displayCal;
+}
+
+set displayCalc(Valor){
+    this._displayCalc = Valor;
+}
+
+get dataAtual(){
+    return this._dataAtual;
+}
+
+set dataAtual(valor){
+    this._datalAtual = valor;
+}
+
 }
