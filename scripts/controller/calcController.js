f49229738000deff0169b5b26f82a9502e496553
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-03-20 19:17:32 -0300

    feat: implement basic operations functionality in calculator

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -86,11 +86,29 @@ class CalcController {
 
     this._operation = [result, last];
 
+    this.setLastNumberToDisplay();
+
 }
 
       setLastNumberToDisplay(){
 
+        let lastNumber;
+
+        for (let i = this._operation.length-1; i >= 0; i--){
+
+            if (!this.isOperator(this._operation[i])){
+
+              lastNumber = this._operation[i];
+
+                break;
+
+        }
+
    }
+
+   this.displayCalc = lastNumber;
+
+  }
 
     addOperation(value){
 
@@ -108,6 +126,8 @@ class CalcController {
         } else {
 
           this.pushOperation(value);
+
+          this.setLastNumberToDisplay();
 
         }
         
