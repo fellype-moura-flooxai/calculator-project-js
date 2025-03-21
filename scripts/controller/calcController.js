42934bf113b91a958ecac3867ec83d8260729d85
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-03-21 17:00:37 -0300

    feat: add double click audio toggle on ac button

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -2,6 +2,9 @@ class CalcController {
 
   constructor(){
 
+
+    this._audio = new Audio('click.mp3');
+    this._audioOnOff = false;
     this._lastOperator = '';
     this._lastNumber = '';
 
@@ -61,12 +64,45 @@ class CalcController {
     this.setLastNumberToDisplay();
     this.pasteFromClipboard();
 
+    document.querySelectorAll('.btn-ac').forEach(btn =>{
+
+      btn.addEventListener('dblclick', e=>{
+
+        this.toggleAudio();
+
+
+      })
+
+    })
+
+
+}
+
+toggleAudio(){
+
+  this._audioOnOff = !this._audioOnOff;
+
+}
+
+playAudio(){
+
+  if (this._audioOnOff) {
+
+    this._audio.currentTime = 0;
+
+    this._audio.play();
+
+
+
+  }
 
 }
 
 initKeyboard(){
 
 document.addEventListener('keyup', e=>{
+
+  this.playAudio();
 
       
     switch (e.key) {
@@ -324,6 +360,8 @@ getLastItem(isOperator = true){
     }
 
     execBtn(value){
+
+      this.playAudio();
       
       switch (value) {
 
