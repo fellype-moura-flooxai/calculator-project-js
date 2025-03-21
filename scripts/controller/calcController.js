e8097ffe7bafca93aa43dc4cd3189c64f5940402
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-03-21 15:40:35 -0300

    fix: resolve bug with multiple dots and remove parsefloat

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -204,7 +204,7 @@ getLastItem(isOperator = true){
         } else {
 
         let newValue = this.getLastOperation().toString() + value.toString();
-        this.setLastOperation(parseFloat(newValue));
+        this.setLastOperation(newValue);
         this.setLastNumberToDisplay();
 
         }
@@ -222,6 +222,9 @@ getLastItem(isOperator = true){
     addDot(){
 
       let lastOperation = this.getLastOperation();
+      
+      if (typeof lastOperation  === 'string' && lastOperation.split('').indexOf('.') > -1) return;
+
       if (this.isOperator(lastOperation) || !lastOperation) {
        this.pushOperation('0.');
       } else {
